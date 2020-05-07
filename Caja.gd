4006a144d7140ab69349beb91c4b29715323c066
Santiago González Villfranca <santyagovilla95@gmail.com>
2020-05-07 11:53:10 +0200

    Supply Box added
    Se ha añadido una escena 'Caja' en el que, espeficando el tipo junto a otros parametros, sirve de suministros para los atributos del Player

---
+++ Caja.gd
@@ -0,0 +1,41 @@
+extends Area
+
+enum TIPO { MUNICION, VIDA, ENERGIA}
+export(TIPO) var tipo = TIPO.VIDA
+export var quantity = 1
+
+# Referencia a matilda
+onready var player = get_node("/root/WorldMap/Navigation/Matilda") 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if tipo == TIPO.VIDA:
+		print("Vida")
+		# TODO: MeshInstance, animation, etc..
+	elif tipo == TIPO.MUNICION:
+		print("Municion")
+		# TODO: MeshInstance, animation, etc..
+	elif tipo == TIPO.ENERGIA:
+		print("Energia")
+		# TODO: MeshInstance, animation, etc..
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_Caja_body_entered(body):
+	#	print("bullet collision!")
+	if(body==player):
+		print("Caja entered")
+		
+		if tipo == TIPO.VIDA:
+			# TODO:
+			player.take_health(quantity)
+		elif tipo == TIPO.MUNICION:
+			player.take_ammunition(quantity)
+			# TODO:
+		elif tipo == TIPO.ENERGIA:
+			player.take_energy(quantity)
+			#TODO:
+		
+		# TODO: Animacion desaparer
+		queue_free()
--- personajes/Matilda/prefab/Matilda.gd
+++ personajes/Matilda/prefab/Matilda.gd
@@ -2,13 +2,18 @@ extends KinematicBody
 
 var LineDrawer = preload("res://Personajes/Matilda/prefab/DrawLine3D.gd").new()
 
-# stats (Vida de los enemigos)
-var curHp : int = 3
-var maxHp : int = 3
+# Health points
+export var maxHp : int = 3
+var curHp : int = maxHp
+# Energy points
+export var maxEp : int = 3
+var curEp : int = maxEp
+# Ammunition points
+export var maxAp : int = 3
+var curAp : int = maxAp
 
 var path = []
 var path_ind = 0
-var target = Vector3(0,1,0)
 const move_speed = 2
 onready var nav = get_parent()
 
@@ -46,9 +51,8 @@ func _physics_process(delta):
 	# Ha llegado a destino
 	if path_ind == path.size() and !muerta:
 		pararse()
-		
+#		
 func move_to(target_pos):
-	target = target_pos
 	path = nav.get_simple_path(global_transform.origin, target_pos,true)
 	path_ind = 0
 	andar()	
@@ -63,17 +67,31 @@ func move_to(target_pos):
 
 	# Create a proper curve from the path (¿Create Smoothed Version?)
 	path = curve.get_baked_points()
-
 		
-# called when the enemy deals damage to matilda
+# Funciones Interfaz
 func take_damage(damageToTake):
 #	print("Matilda: AU!!!")
 	curHp -= damageToTake
+	print("Healthy Point: " + str(curHp))
 	# if our health reaches 0 - die
 	if curHp <= 0:
 		morir();
 				
-# called when our health reaches 0
+func take_health(healthToTake):
+	if curHp < maxHp:
+		curHp += healthToTake
+	print("Healthy Point: " + str(curHp))	
+	
+func take_energy(energyToTake):
+	if curEp < maxEp:
+		curEp =+ energyToTake
+
+func take_ammunition(ammunitionToTake):
+	if curAp < maxAp:
+		curAp =+ ammunitionToTake				
+
+
+# Funciones de Animacion
 func morir():
 	print("Matilda: Me muero :(")
 	estado=ESTADOS.muriendo
